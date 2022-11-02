f6ffc7457da89e44c666f09e57cf656451ca1865
Andres Perez <edgar4698@hotmail.com>
2022-11-02 03:13:57 -0500

    Página Zoom-Range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
@@ -10,6 +11,13 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-75.21187326837463,3.2155945571183864 ],
+      zoom: 17
+    });
   }
 
 }
