59466a05eb5cb657171e346e287c5b86fee4f685
Andres Perez <edgar4698@hotmail.com>
2022-11-02 22:43:39 -0500

    guardar y leer los marcadores desde el localstorage

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,15 +1,11 @@
-import {
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-  AfterViewInit,
-} from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import { map } from 'rxjs';
 
 interface iMarcadorColor {
   color: string;
-  marker: mapboxgl.Marker;
+  marker?: mapboxgl.Marker;
+  centro?: [number, number];
 }
 @Component({
   selector: 'app-marcadores',
@@ -33,7 +29,7 @@ export class MarcadoresComponent implements AfterViewInit {
       center: this.center,
       zoom: this.zoomLevel,
     });
-
+    this.leerLocalStorage();
     /* Creación de un marcador en el mapa. */
     // const markerHtml: HTMLElement = document.createElement('div');
     // markerHtml.innerHTML = 'Hola Mundo';
@@ -46,10 +42,56 @@ export class MarcadoresComponent implements AfterViewInit {
     const nuevoMarcador = new mapboxgl.Marker({ draggable: true, color })
       .setLngLat(this.center)
       .addTo(this.mapa);
+    console.log('nuevo Marcador', nuevoMarcador);
+    this.marcadores.push({ color, marker: nuevoMarcador });
+    this.guardarMarcadoresLS();
 
-      this.marcadores.push({ color , marker: nuevoMarcador});
+    nuevoMarcador.on('dragend', () => {
+      this.guardarMarcadoresLS();
+    });
   }
-  irMarcador() {
-    
+  irMarcador(marker: mapboxgl.Marker) {
+    this.mapa.flyTo({
+      center: marker.getLngLat(),
+    });
+  }
+
+  guardarMarcadoresLS() {
+    const lngLatArr: iMarcadorColor[] = [];
+    this.marcadores.forEach((m) => {
+      const color = m.color;
+      const { lng, lat } = m.marker!.getLngLat();
+
+      lngLatArr.push({
+        color: color,
+        centro: [lng, lat],
+      });
+    });
+
+    /* Guardar los marcadores en el almacenamiento local. tener en cuenta que solo se almacenan datos de tipo string  */
+    localStorage.setItem('marcadores', JSON.stringify(lngLatArr));
+  }
+
+  leerLocalStorage() {
+    if (!localStorage.getItem('marcadores')) {
+      return;
+    }
+
+    const lngLatArr: iMarcadorColor[] = JSON.parse(
+      localStorage.getItem('marcadores')!
+    );
+    lngLatArr.forEach((m) => {
+      const newMarker = new mapboxgl.Marker({
+        color: m.color,
+        draggable: true,
+      })
+        .setLngLat(m.centro!)
+        .addTo(this.mapa);
+
+      this.marcadores.push({
+        marker: newMarker,
+        color: m.color,
+      });
+    });
   }
 }
